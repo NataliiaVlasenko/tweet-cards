092e074fe3861d4f5a5d975fca14de567ea88eae
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2023-06-02 00:52:18 +0300

    Deploy React app to GitHub Pages

--- static/js/680.ca122195.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktweet_cards=self.webpackChunktweet_cards||[]).push([[680],{680:function(e,t,c){c.r(t);var n=c(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Home"})})}}}]);
-//# sourceMappingURL=680.ca122195.chunk.js.map
